1793cd04b87236038963d2c5b6cda3fa6737444d
velimd <velican.duzgunce@solirius.com>
2019-12-09 10:11:18 +0000

    form 2

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function contactUs() {
   const data = document.getElementById("frm1");
   console.log(data);
   var text = "";
-  var i;
-  for (i = 0; i < data.length-1 ;i++) {
+  for (var i = 0; i < data.length-1 ;i++) {
     text += data.elements[i].value + "<br>";
   }
   document.getElementById("displayForm").innerHTML = text;
